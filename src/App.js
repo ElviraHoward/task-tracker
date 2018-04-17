92a950e6013821fe5abd5068708974414feac4ee
Irina Kazantseva <irina.kazantseva@waveaccess.ru>
2018-04-17 20:16:44 +0400

    update table

--- src/App.js
+++ src/App.js
@@ -1,62 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const TABLE_COLUMNS = [
-    {
-        label: 'ID',
-    },{
-        label: 'Description',
-    },{
-        label: 'Status',
-    }, {
-        label: 'Action',
-    }
-];
-
-const tableData = [
-    [1, "blabla", "in progress", <button onClick={(e)=> this.fDelete(e)} className="DelBtn">Delete</button>, <button onClick={(e)=> this.fSubmit(e)} className="EditBtn">Edit</button>],
-    [2, "blabla", "resolved", <button onClick={(e)=> this.fDelete(e)} className="DelBtn">Delete</button>, <button onClick={(e)=> this.fSubmit(e)} className="EditBtn">Edit</button>],
-    [3, "blabla", "feedback", <button onClick={(e)=> this.fDelete(e)} className="DelBtn">Delete</button>, <button onClick={(e)=> this.fSubmit(e)} className="EditBtn">Edit</button>],
-    [4, "blabla", "closed", <button onClick={(e)=> this.fDelete(e)} className="DelBtn">Delete</button>, <button onClick={(e)=> this.fSubmit(e)} className="EditBtn">Edit</button>],
-];
-
-const TableHeader = (props) => {
-    return(
-        <thead>
-        <tr>
-            {TABLE_COLUMNS.map((element, index) =>
-                <th key={index}>{element.label}</th>
-            )}
-        </tr>
-        </thead>
-    )
-};
-
-class TableBody extends Component {
-    render () {
-        return (
-            <tbody>
-            {tableData.map((element, index) =>
-                <tr key={index+1}>
-                    {element.map((item, i) =>
-                        <td>{item}</td>
-                    )}
-                </tr>
-            )}
-            </tbody>
-        )
-    }
-}
 class App extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            datas: [],
-            column: TABLE_COLUMNS,
+            head_names: ['id', 'description', 'status', 'actions'],
+            rows: [
+                [1, "blabla", <Status />, <button onClick={(e)=> this.fDelete(e)} className="DelBtn">Delete</button>, <button onClick={(e)=> this.fSubmit(e)} className="EditBtn">Edit</button>],
+                [2, "blabla", <Status />, <button onClick={(e)=> this.fDelete(e)} className="DelBtn">Delete</button>, <button onClick={(e)=> this.fSubmit(e)} className="EditBtn">Edit</button>],
+                [3, "blabla", <Status />, <button onClick={(e)=> this.fDelete(e)} className="DelBtn">Delete</button>, <button onClick={(e)=> this.fSubmit(e)} className="EditBtn">Edit</button>],
+                [4, "blabla", <Status />, <button onClick={(e)=> this.fDelete(e)} className="DelBtn">Delete</button>, <button onClick={(e)=> this.fSubmit(e)} className="EditBtn">Edit</button>],
+            ]
         }
+    }
+
+    AddRow() {
+        let newRows = this.state.rows;
+        newRows.push();
+        this.setState({rows: newRows});
     }
 
     fSubmit = (e) => {
@@ -100,13 +64,84 @@ class App extends Component {
           </form>
           <button onClick={(e)=> this.fSubmit(e)} className="AddBtn">ADD</button>
           <h1 className="App-title">Task tracker</h1>
-        <table  border="1">
-            <TableHeader columns={this.state.column} />
-            <TableBody data={this.state.data} />
-        </table>
+          <Table head={this.state.head_names} rows={this.state.rows} />
       </div>
     );
   }
+}
+
+class Table extends Component {
+    render() {
+        return (
+            <table border="1">
+                <thead>
+                {this.genHead()}
+                </thead>
+                <tbody>
+                {this.genRow()}
+                </tbody>
+            </table>
+        );
+    }
+
+    genHead() {
+        var head = this.props.head;
+
+        return head.map(function(v, i) {
+            return (
+                <th key={'th' + i}>
+                    {v}
+                </th>
+            );
+        });
+    }
+
+    genRow() {
+        var rows = this.props.rows;
+
+        return rows.map(function(v, i) {
+            var tmp = v.map(function(v2, j) {
+                return (
+                    <td key={'td' + i + '_' + j}>
+                        {v2}
+                    </td>
+                );
+            });
+
+            return (
+                <tr key={'tr' + i}>
+                    {tmp}
+                </tr>
+            )
+        });
+    }
+}
+class Status extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 'in progress'};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="in progress">In progress</option>
+                        <option value="resolved">Resolved</option>
+                        <option value="closed">Closed</option>
+                        <option value="feedback">Feedback</option>
+                    </select>
+                </label>
+            </form>
+        );
+    }
 }
 
 export default App;
